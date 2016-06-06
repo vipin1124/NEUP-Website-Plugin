46846e594b0d96f232587ab990eec67bf4107e03
NightSiesta <qwer3582919@sina.cn>
2016-06-06 09:37:42 +0800

    Update NightSiesta_plugin.php

--- NightSiesta_plugin.php
+++ NightSiesta_plugin.php
@@ -11,13 +11,13 @@ License: GPL
 ?>
 <?php
 if( is_admin() ) {
-    /*  ÀûÓÃ admin_menu ¹³×Ó£¬Ìí¼Ó²Ëµ¥ */
+    /*  脌没脫脙 admin_menu 鹿鲁脳脫拢卢脤铆录脫虏脣碌楼 */
     add_action('admin_menu', 'display_NightSiesta_menu');
 }
 
 function display_NightSiesta_menu() {
     /* add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);  */
-    /* Ò³Ãû³Æ£¬²Ëµ¥Ãû³Æ£¬·ÃÎÊ¼¶±ð£¬²Ëµ¥±ðÃû£¬µã»÷¸Ã²Ëµ¥Ê±µÄ»Øµ÷º¯Êý£¨ÓÃÒÔÏÔÊ¾ÉèÖÃÒ³Ãæ£© */
+    /* 脪鲁脙没鲁脝拢卢虏脣碌楼脙没鲁脝拢卢路脙脦脢录露卤冒拢卢虏脣碌楼卤冒脙没拢卢碌茫禄梅赂脙虏脣碌楼脢卤碌脛禄脴碌梅潞炉脢媒拢篓脫脙脪脭脧脭脢戮脡猫脰脙脪鲁脙忙拢漏 */
     add_options_page('Set NightSiesta', 'NightSiesta Menu', 'administrator','display_nightsiesta', 'display_NightSiesta_html_page');    
 }
 function display_NightSiesta_html_page()
@@ -100,19 +100,26 @@ function display_NightSiesta_html_page()
 	$link3= $l->option_value;   
 	?>
 	<p>Title1<input type="text" id="title_post1" name="title1" value="<?php echo("$title1") ?>"></p>
-	<p>Time1<input class="form-control" name="time1" type="datetime-local" id="time_post1" value="<?php echo date("Y-m-d H:i:s", $time1) ?>"></p>
+	<p>Time1<input class="form-control" name="time1" type="datetime-local" id="time_post1" value="<?php echo str_replace("","T", "$time1") ?>"></p>
 	<p>Place1<input type="text" id="place_post1" name="place1" value="<?php echo("$place1") ?>"></p>
 	<p>Link1<input type="text" id="link_post1" name="link1" value="<?php echo("$link1") ?>"></p>
 	<p>Title2<input type="text" id="title_post2" name="title2" value="<?php echo("$title2") ?>"></p>
-	<p>Time2<input class="form-control" name="time2" type="datetime-local" id="time_post2" value="<?php echo date("Y-m-d H:i:s", $time1) ?>"></p>
+	<p>Time2<input class="form-control" name="time2" type="datetime-local" id="time_post2" value="<?php echo str_replace("","T", "$time2") ?>"></p>
 	<p>Place2<input type="text" id="place_post2" name="place2" value="<?php echo("$place2") ?>"></p>
 	<p>Link2<input type="text" id="link_post2" name="link2" value="<?php echo("$link2") ?>"></p>
 	<p>Title3<input type="text" id="title_post3" name="title3" value="<?php echo("$title3") ?>"></p>
-	<p>Time3<input class="form-control" name="time3" type="datetime-local" id="time_post3" value="<?php echo date("Y-m-d H:i:s", $time1)?>"></p>
+	<p>Time3<input class="form-control" name="time3" type="datetime-local" id="time_post3" value="<?php echo str_replace("","T", "$time1") ?>"></p>
 	<p>Place3<input type="text" id="place_post3" name="place3" value="<?php echo("$place3") ?>"></p>
 	<p>Link3<input type="text" id="link_post3" name="link3" value="<?php echo("$link3") ?>"></p>
 	<input type="submit" value="submit"></input>
 	<input type="hidden" name="submited" value="true"></input>
 	</form></br>
 	<?php
+}
+add_filter('the content','show_index');
+/*下面是插件在主页的实现*/
+function show_index()
+{
+	if(is_home())
+	return $title1,$title2,$title3,$time1,$time2,$time3,$place1,$place2,$place3,$link1,$link2,$link3;
 }
